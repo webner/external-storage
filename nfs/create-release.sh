dd5978b00edc1d8184067ee03514c656e4c2b986
Wolfgang Ebner <wolfgang.ebner@catalysts.cc>
2020-02-28 08:08:40 +0100

    update deploy script

--- nfs/create-release.sh
+++ nfs/create-release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-make
-tar czf nfs-provisioner-v1.0.4.cat.tar.gz nfs-provisioner tp-free deploy/kubernetes deploy/systemd
+#make
+tar czf nfs-provisioner-v1.0.5.cat.tar.gz nfs-provisioner tp-free deploy/kubernetes deploy/systemd
